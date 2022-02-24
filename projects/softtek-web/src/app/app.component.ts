20232231175ccdeaf530bf5220c561c17e6a449c
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-24 10:02:15 +0100

    feat: ng application softtek-web

---
+++ projects/softtek-web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stk-web-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'softtek-web';
+}
