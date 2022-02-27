1e302abff15be430337cdb4b98feecf2c2d76c94
Alberto Basalo <albertobasalo@hotmail.com>
2022-02-27 12:20:34 +0100

    feat: template reference

--- projects/softtek-web/src/app/shared/async-wrapper/async-wrapper.component.ts
+++ projects/softtek-web/src/app/shared/async-wrapper/async-wrapper.component.ts
@@ -8,22 +8,10 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AsyncWrapperComponent implements OnInit {
+  @Input() dataTemplate: TemplateRef<any>;
   @Input() dataName: string = '';
 
   @Input() data$: Observable<unknown[]> | undefined;
-  // @Input() set data(value: Observable<unknown[]> | undefined) {
-  //   if (!value) return;
-  //   console.log('AsyncWrapperComponent.data', value);
-  //   value.pipe(
-  //     tap({
-  //       error: (err) => {
-  //         console.log('AsyncWrapperComponent.data.error', err);
-  //         this.error$.next(err.message);
-  //       },
-  //     })
-  //   );
-  //   this.data$ = value;
-  // }
 
   @Input() error$: Observable<string> | undefined;
   constructor() {}
